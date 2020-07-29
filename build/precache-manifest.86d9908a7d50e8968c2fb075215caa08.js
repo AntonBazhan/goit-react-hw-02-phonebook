197d95fa44f7f9cd070a5645453020b2192a8530
Anton <bazhan.anton@ukr.net>
2020-07-29 14:07:35 +0300

    add build

---
+++ build/precache-manifest.86d9908a7d50e8968c2fb075215caa08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf33ac925c1f076ca4332b0e12a34be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f36a5f0fc506d700e1c",
+    "url": "/static/css/main.c6d6b915.chunk.css"
+  },
+  {
+    "revision": "f4151cca25f84c0fd69f",
+    "url": "/static/js/2.14b0ad3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.14b0ad3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f36a5f0fc506d700e1c",
+    "url": "/static/js/main.730fb3a0.chunk.js"
+  },
+  {
+    "revision": "4ff1f0d5bce29dab4e96",
+    "url": "/static/js/runtime-main.3ce725fe.js"
+  }
+]);
